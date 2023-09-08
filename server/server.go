3d5993718b7cafd800e8f1a346c2dd0fce7bac3f
FantasticCowboy <lukehobeika@gmail.com>
2023-09-08 06:37:13 -0400

    added changes

--- server/server.go
+++ server/server.go
@@ -1,8 +1,8 @@
 package server
 
 import (
-	"fmt"
 	"lamport_demo/constants"
+	"lamport_demo/io/file"
 	"lamport_demo/io/network"
 	"log"
 	"os"
@@ -60,50 +60,7 @@ func CreateNewServer(ip string) (*Server, error) {
 	srv.messagesReceived = make([]network.Message, 0)
 	srv.lock = new(sync.Mutex)
 	log.Printf("Server created at %s:%s", ip, constants.SERVER_PORT)
-
-	programDone := make(chan os.Signal, 1)
-	signal.Notify(programDone, syscall.SIGTERM, syscall.SIGINT)
-
-	go func(srv *Server) {
-		<-programDone
-		srv.lock.Lock()
-		defer srv.lock.Unlock()
-		sort.Slice(srv.messagesReceived,
-			func(i, j int) bool {
-				return (srv.messagesReceived[i].LamportClock < srv.messagesReceived[j].LamportClock ||
-					(srv.messagesReceived[i].LamportClock == srv.messagesReceived[j].LamportClock &&
-						srv.messagesReceived[i].SenderId < srv.messagesReceived[j].SenderId))
-			})
-
-		file.WriteMessagesToFile()
-		f, err := os.Create("./sorted_by_lamport_clock.txt")
-		if err != nil {
-			log.Fatalf("Couldn't open file!")
-		}
-
-		f.WriteString(fmt.Sprintf("Sender Id, wall clock, lamport clock, message\n"))
-		for _, val := range srv.messagesReceived {
-			f.WriteString(fmt.Sprintf("%d,%v,%v,%s", val.SenderId, val.WallClock, val.LamportClock, val.Text))
-		}
-		f.Close()
-
-		sort.Slice(srv.messagesReceived,
-			func(i, j int) bool {
-				return (srv.messagesReceived[i].WallClock.Before(srv.messagesReceived[j].WallClock))
-			})
-
-		f, err = os.Create("./sorted_by_timestamp.txt")
-		if err != nil {
-			log.Fatalf("Couldn't open file!")
-		}
-		f.WriteString(fmt.Sprintf("Sender Id, wall clock, lamport clock, message\n"))
-		for _, val := range srv.messagesReceived {
-			f.WriteString(fmt.Sprintf("%d,%v,%v,%s", val.SenderId, val.WallClock, val.LamportClock, val.Text))
-		}
-		f.Close()
-		os.Exit(0)
-	}(&srv)
-
+	srv.registerTeardown()
 	return &srv, err
 }
 
@@ -111,11 +68,43 @@ func (srv *Server) ProcessMessages() {
 	log.Printf("Server processing messages")
 	for {
 		message := <-srv.incomingMessages
+		srv.lock.Lock()
+		defer srv.lock.Unlock()
 		srv.updateMaxClockValueSeen(message.LamportClock)
 		log.Printf("Received message: %+v", message)
 		go srv.PingClient(message.SenderIpAddress, srv.getMaxClockValueSeen())
-		srv.lock.Lock()
 		srv.messagesReceived = append(srv.messagesReceived, message)
-		srv.lock.Unlock()
 	}
+}
+
+func (srv *Server) registerTeardown() {
+	programDone := make(chan os.Signal, 1)
+	signal.Notify(programDone, syscall.SIGTERM, syscall.SIGINT)
+
+	go func(srv *Server) {
+		<-programDone
+		srv.lock.Lock()
+		defer srv.lock.Unlock()
+		srv.sortMessagesByLamportClock(srv.messagesReceived)
+		file.WriteMessagesToFile("./sorted_by_lamport_clock.txt", srv.messagesReceived)
+		srv.sortMessagesByTimestamp(srv.messagesReceived)
+		file.WriteMessagesToFile("./sorted_by_timestamp.txt", srv.messagesReceived)
+		os.Exit(0)
+	}(srv)
+}
+
+func (srv *Server) sortMessagesByTimestamp(messages []network.Message) {
+	sort.Slice(srv.messagesReceived,
+		func(i, j int) bool {
+			return (srv.messagesReceived[i].WallClock.Before(srv.messagesReceived[j].WallClock))
+		})
+}
+
+func (srv *Server) sortMessagesByLamportClock(messages []network.Message) {
+	sort.Slice(srv.messagesReceived,
+		func(i, j int) bool {
+			return (srv.messagesReceived[i].LamportClock < srv.messagesReceived[j].LamportClock ||
+				(srv.messagesReceived[i].LamportClock == srv.messagesReceived[j].LamportClock &&
+					srv.messagesReceived[i].SenderId < srv.messagesReceived[j].SenderId))
+		})
 }
